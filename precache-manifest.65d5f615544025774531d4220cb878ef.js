a67744d64c4fb41b9b8d2ed54aa7151ba067b7b9
m9d9v9 <m9d9v9@hotmail.com>
2020-12-09 16:24:15 -0700

    Updates

---
+++ precache-manifest.65d5f615544025774531d4220cb878ef.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c4b956e531c47fd7af72398e53024156",
+    "url": "/MetodoG/index.html"
+  },
+  {
+    "revision": "d52938918dd3b4456736",
+    "url": "/MetodoG/static/css/2.1832cc2a.chunk.css"
+  },
+  {
+    "revision": "1efd9bd174db68c5d4cb",
+    "url": "/MetodoG/static/css/main.ce4ca2af.chunk.css"
+  },
+  {
+    "revision": "d52938918dd3b4456736",
+    "url": "/MetodoG/static/js/2.2df7eab8.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/MetodoG/static/js/2.2df7eab8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1efd9bd174db68c5d4cb",
+    "url": "/MetodoG/static/js/main.21647997.chunk.js"
+  },
+  {
+    "revision": "5a933eff7d88593f909b",
+    "url": "/MetodoG/static/js/runtime-main.d8cc4424.js"
+  }
+]);
